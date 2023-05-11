d27e38acfe4adee375a27ae0384b2f066381c280
AgustinSileoniUNC <agustinsileoni@mi.unc.edu.ar>
2023-05-11 13:21:05 -0300

    Agregado el mock de skills

---
+++ src/app/Skill_item.ts
@@ -0,0 +1,5 @@
+export interface skills_item{
+    id:number,
+    nombre:string,
+    porcentaje:number
+}
---
+++ src/app/mock-skills.ts
@@ -0,0 +1,24 @@
+import { skills_item } from "./Skill_item"
+
+export const skills:skills_item[]=[
+    {
+        id:1,
+        nombre:"HTML & CSS",
+        porcentaje:65
+    },
+    {
+        id:2,
+        nombre:"Javascript",
+        porcentaje:70
+    },
+    {
+        id:3,
+        nombre:"React JS",
+        porcentaje:60
+    },
+    {
+        id:4,
+        nombre:"Angular",
+        porcentaje:75
+    }
+]
